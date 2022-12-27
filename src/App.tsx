b8b63ce16c4553e7c8e1c744dd03d723ba37b3cb
katy-solo <katy-solo@yandex-team.ru>
2022-12-27 13:28:34 +0500

    redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./redux/hooks";
+import {increaseCounter} from "./redux/reducers/form";
 
-function App() {
+const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(state => state.form.counter);
+
   return (
-    <div className="App" />
+    <div className="App">
+      Current count is {count}
+      <button onClick={() => dispatch(increaseCounter())}>Click me</button>
+    </div>
   );
 }
 
