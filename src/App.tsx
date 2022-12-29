abd506b5967a8d40c59407bcc8e8c3686acc4611
katy-solo <katy-solo@yandex-team.ru>
2022-12-29 16:54:09 +0500

    menu sidebar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom'
+import { Menu, Segment, Sidebar } from 'semantic-ui-react'
 import styles from './App.module.css'
 import { AccessFormView } from './views/AccessFormView'
 import { Header } from './components/Header'
@@ -11,16 +12,48 @@ export function classNames (...classNames: Array<string | false | null | undefin
 }
 
 const App: React.FC = () => {
+  const [visible, setVisible] = React.useState(false)
+
   return (
         <Router>
-            <Header />
-            <div className={styles.app}>
-                <Routes>
-                    <Route path="/" element={<AccessFormView/>}/>
-                    <Route path="/add/person" element={<AddEmployeeView />}/>
-                    <Route path="/add/department" element={<AddDepartmentView />} />
-                </Routes>
-            </div>
+            <Sidebar.Pushable as={Segment}>
+                <Sidebar
+                    as={Menu}
+                    animation='overlay'
+                    icon='labeled'
+                    inverted
+                    onHide={() => setVisible(false)}
+                    vertical
+                    visible={visible}
+                    width='thin'
+                >
+                    <Menu.Item as='div'>
+                        <Link to={'/'}>
+                            Добавить отметку
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item as='div'>
+                        <Link to={'/add/department'}>
+                            Добавить департамент
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item as='div'>
+                        <Link to={'/add/person'}>
+                            Добавить сотрудника
+                        </Link>
+                    </Menu.Item>
+                </Sidebar>
+                <Sidebar.Pusher dimmed={visible}>
+                    <Header onMenuClick={() => setVisible(true)}/>
+                    <div className={styles.app}>
+                        <Routes>
+                            <Route path="/" element={<AccessFormView/>}/>
+                            <Route path="/add/person" element={<AddEmployeeView/>}/>
+                            <Route path="/add/department" element={<AddDepartmentView/>}/>
+                        </Routes>
+                    </div>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
         </Router>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 import styles from './Header.module.css'
 
-export const Header: React.FC = () => {
+export const Header: React.FC<{ onMenuClick: () => void }> = ({ onMenuClick }) => {
   return (
         <div className={styles.header}>
-
+            <Button onClick={onMenuClick} size={'huge'}>Меню</Button>
         </div>
   )
 }
